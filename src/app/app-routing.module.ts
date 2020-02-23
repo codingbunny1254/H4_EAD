2568b11557d7ef41b2c9a4918ba8f33d2c3ded9e
unknown <codingbunny1254@gmail.com>
2020-02-22 18:04:35 -0600

    Added new route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ApplicationFormComponent} from '../app/Components/application-form/application-form.component';
 
 
-const routes: Routes = [
-  
-];
+const routes: Routes = [{path: 'applicationForm',
+component: ApplicationFormComponent,
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
