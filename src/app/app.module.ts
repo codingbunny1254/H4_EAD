da5bf205d93e77e2fff44b7b816372660cd6713b
unknown <codingbunny1254@gmail.com>
2020-03-07 16:41:48 -0600

    fixed routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ApplicationFormComponent } from './Components/application-form/application-form.component';
 import { LoginComponent } from './Components/login/login.component';
-import { ReasonAndPersonalDetailsComponent } from './reason-and-personal-details/reason-and-personal-details.component';
-import { MailingAndPhysicalAddressDetailsComponent } from './mailing-and-physical-address-details/mailing-and-physical-address-details.component';
-import { OtherInformationComponent } from './other-information/other-information.component';
-import { OtherInformationPart2Component } from './other-information-part2/other-information-part2.component';
-import { OtherInformationPart3Component } from './other-information-part3/other-information-part3.component';
+import { ReasonAndPersonalDetailsComponent } from './Components/reason-and-personal-details/reason-and-personal-details.component';
+import { MailingAndPhysicalAddressDetailsComponent } from './Components/mailing-and-physical-address-details/mailing-and-physical-address-details.component';
+import { OtherInformationComponent } from './Components/other-information/other-information.component';
+import { OtherInformationPart2Component } from './Components/other-information-part2/other-information-part2.component';
+import { OtherInformationPart3Component } from './Components/other-information-part3/other-information-part3.component';
 
 @NgModule({
   declarations: [
